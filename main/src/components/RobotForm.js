e1b9135a37d656b368e231acdb3773a054ba2203
anghelcristina <anghelcristina17@stud.ase.ro>
2020-01-15 12:56:24 +0000

    robotform

--- main/src/components/RobotForm.js
+++ main/src/components/RobotForm.js
@@ -1,7 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import RobotStore from '../stores/RobotStore';
 
 class RobotForm extends Component{
     constructor(){
-        super()
+        super();
+        this.name="Bill";
+        this.type="talker";
+        this.mass=980;
+        this.onAdd=function(e){
+            RobotStore.addRobot(this);
+        };
+    }
+    render(){
+        // let item=this.props;
+        return(
+            this   
+        );
     }
 }
